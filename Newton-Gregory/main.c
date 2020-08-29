aaa7564f9bbf8313962ab632e4d2936f185f0fb0
arthurcipolari <arthur.cipolari@gmail.com>
2020-08-29 18:10:01 -0300

    corrigindo newton-gregory

--- Newton-Gregory/main.c
+++ Newton-Gregory/main.c
@@ -1,41 +1,64 @@
-#include<stdio.h>
-#include<conio.h>
-#define MaxN 100
-#define Order_of_diff 3
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define NMAX 100
+
+int fatorial(int n){
+    int i,f=1;
+    for(i=1;i<=n;i++){
+        f=f*i;
+    }
+    return f;
+}
+
+void interpoladorNewtonGregory(float tabela[NMAX][NMAX+1], int nPontos, float pontoX){
+
+    float h, p, y, px=1;
+    int i, j;
+
+    //Montando tabela da Ordem de F
+    for(j=2;j<=nPontos;j++){
+        for(i=0;i<nPontos-1;i++){
+        tabela[i][j]=tabela[i+1][j-1]-tabela[i][j-1];
+        }
+    }
+
+    //Calculo de f(pontoX)
+    h=tabela[1][0]-tabela[0][0];
+    p=(pontoX-tabela[0][0])/h;
+    y=tabela[0][1];
+    for(i=1;i<nPontos;i++){
+        px=px*(p-(i-1));
+        y=y+(tabela[0][i+1]*px)/fatorial(i);
+    }
+
+    printf("\n\nO valor de f(%2.4f) e' de: %2.4f\n", pontoX, y);
+
+}
 
 int main(void){
- float arr_x[MaxN+1], arr_y[MaxN+1], numerator=1.0, denominator=1.0, x, y, p, h, diff_table[MaxN+1][Order_of_diff+1];
- int i,j,n,k;
 
- printf("Enter the value of n n");
- scanf("%d",&n);
- printf("Enter the values of x and y");
+    float tabela[NMAX][NMAX+1], pontoX;
+    int i, nPontos;
 
- for(i=0; i<=n; i++)
-  scanf("%f%f", &arr_x[i], &arr_y[i]);
- printf("Enter the value of x at which value of y is to be calculated");
- scanf("%f", &x);
- h=arr_x[1]-arr_x[0];
+    printf("\nDigite o numero de pontos na tabela: ");
+    scanf("%d",&nPontos);
 
- for(i=0; i<=n-1; i++)
-  diff_table[i][1]=arr_y[i+1]-arr_y[i];/*Creating the difference table and calculating first order differences*/
- for(j=2; j<=Order_of_diff; j++)/*Calculating higher order differences*/
-  for(i=0; i<=n-j; i++)
-   diff_table[i][j]=diff_table[i+1][j-1] - diff_table[i][j-1];
- i=0;
+    printf("\nDigite os valores de x e y(Ex: 1 5): \n");
+    for(i=0; i<nPontos; i++){
+        scanf ("%f",&tabela[i][0]);
+        scanf("%f",&tabela[i][1]);
+    }
 
- while(!(arr_x[i]>x)) /* Finding x0 */
-  i++;
- i--;
- p=(x-arr_x[i])/h;
- y=arr_y[i];
+    printf("\nTabela digitada :\n");
+    printf("-----X------Y------\n");
+    for(i=0; i<nPontos; i++){
+        printf("| %2.4f  %2.4f  |\n",tabela[i][0],tabela[i][1]);
+    }
+    printf("-------------------");
 
- for (k=1; k<=Order_of_diff; k++)
- {
-  numerator *=p-k+1;
-  denominator *=k;
-  y +=(numerator/denominator)*diff_table[i][k];
- }
- printf("When x=%6.1f, y=%6.2fn",x, y);
- getch();
+    printf("\n\nDigite o valor de X para encontrar o respectivo Y: ");
+    scanf("%f",&pontoX);
+
+    interpoladorNewtonGregory(tabela, nPontos, pontoX);
 }
